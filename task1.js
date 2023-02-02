cc449caa1b6e290e85ab4b028bc4301ad67f05b6
apandis94 <apandi.saputra94@gmail.com>
2023-02-02 17:27:16 +0700

    Edit kondisi negatif

--- task1.js
+++ task1.js
@@ -5,12 +5,12 @@ const nilai = prompt("Masukan nilai: ");
 var y = nilai;
 var x = y;
 
-if (x % 2 == 0) {
+if (nilai < 0) {
+  console.log("Tidak bisa input bilangan negatif");
+} else if (x % 2 == 0) {
   console.log("hasil akar pangkat dua dari angka " + y + " adalah = " + Math.sqrt(x));
 } else if (x % 2 != 0) {
   console.log("Tidak bisa input bilangan ganjil");
-} else if (x < 0) {
-  console.log("Tidak bisa input bilangan negatif");
 } else {
   return;
 }
