62a2ee7f5c873e072ca8709db6d6ce1bfdd193da
apandis94 <apandi.saputra94@gmail.com>
2023-02-02 09:13:46 +0700

    Perubahan Task1 akar pangkat 2

--- task1.js
+++ task1.js
@@ -2,10 +2,11 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 const nilai = prompt("Masukan nilai: ");
 
-var x = nilai * nilai;
+var y = nilai;
+var x = y;
 
 if (x % 2 == 0) {
-  console.log("hasil akar dua dari angka yang adalah = " + x);
+  console.log("hasil akar pangkat dua dari angka " + y + " adalah = " + Math.sqrt(x));
 } else if (x % 2 != 0) {
   console.log("Tidak bisa input bilangan ganjil");
 } else if (x < 0) {
